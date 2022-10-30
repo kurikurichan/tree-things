55306bbd0de907c4443b092f507641b034bf2691
NF Devs <nonfungibledevs@gmail.com>
2022-10-30 13:14:05 -0700

    success message

--- components/AddSessionModal.js
+++ components/AddSessionModal.js
@@ -3,6 +3,7 @@ import { useSession } from "next-auth/react";
 import { startSession } from "mongoose";
 
 const AddSessionModal = ({ setShowModal }) => {
+  const [success, setSuccess] = useState(null);
   const [userData, setUserData] = useState({
     description: "",
     imageUrl:
@@ -35,6 +36,7 @@ const AddSessionModal = ({ setShowModal }) => {
       });
 
       console.log(res);
+      setSuccess(true);
       setShowModal(false);
     } catch (error) {
       console.error(error);
@@ -48,20 +50,41 @@ const AddSessionModal = ({ setShowModal }) => {
           className="fixed inset-0 w-full h-full bg-black opacity-40"
           onClick={() => setShowModal(false)}
         ></div>
-        <div className="flex items-center min-h-screen px-4 py-8">
-          <div className="relative max-w-lg mx-auto bg-white rounded-md shadow-lg">
-            <h4 className="text-lg p-4  bg-indigo-600 font-medium mb-4 text-center text-indigo-100">
-              Add New Tree Report?
-            </h4>
-            <div className="mt-3 p-4 sm:flex">
-              <div className="mt-2 text-center sm:ml-4 sm:text-left">
-                <div className="flex justify-center flex-col space-y-3">
-                  <span className="text-lg text-center mb-2 font-extrabold text-gray-700">
-                    {" "}
-                    Hi! name {session.user?.firstName}
-                  </span>
+        {success ? (
+          <SuccessForm
+            session={session}
+            handleSubmit={handleSubmit}
+            totalTrees={totalTrees}
+          />
+        ) : (
+          <SubmissionForm
+            session={session}
+            setUserdata={setUserData}
+            userData={userData}
+          />
+        )}
+      </div>
+    </>
+  );
+};
 
-                  {/* <div>
+export default AddSessionModal;
+
+export const SubmissionForm = ({ session, setUserdata, userData }) => {
+  <div className="flex items-center min-h-screen px-4 py-8">
+    <div className="relative max-w-lg mx-auto bg-white rounded-md shadow-lg">
+      <h4 className="text-lg p-4  bg-indigo-600 font-medium mb-4 text-center text-indigo-100">
+        Add New Tree Report?
+      </h4>
+      <div className="mt-3 p-4 sm:flex">
+        <div className="mt-2 text-center sm:ml-4 sm:text-left">
+          <div className="flex justify-center flex-col space-y-3">
+            <span className="text-lg text-center mb-2 font-extrabold text-gray-700">
+              {" "}
+              Hi! name {session.user?.firstName}
+            </span>
+
+            {/* <div>
                     <label
                       htmlFor="upload_photo"
                       className="block px-3 py-2 overflow-hidden border border-gray-200 rounded-md shadow-sm focus-within:ring-1 focus-within:ring-zinc-600 focus-within:border-zinc-600"
@@ -100,90 +123,115 @@ const AddSessionModal = ({ setShowModal }) => {
                     </label>
                   </div> */}
 
-                  <label
-                    htmlFor="treeAmount"
-                    className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-                  >
-                    <span className="text-xs font-medium text-gray-700">
-                      {" "}
-                      How many tree's were planted?
-                    </span>
+            <label
+              htmlFor="treeAmount"
+              className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+            >
+              <span className="text-xs font-medium text-gray-700">
+                {" "}
+                How many tree's were planted?
+              </span>
 
-                    <input
-                      type="text"
-                      id="treeAmount"
-                      onChange={(e) =>
-                        setUserData({ ...userData, totalTrees: e.target.value })
-                      }
-                      placeholder="Enter tree planted amount?"
-                      className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-                    />
-                  </label>
-                  <label
-                    htmlFor="treeAmount"
-                    className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-                  >
-                    <span className="text-xs font-medium text-gray-700">
-                      {" "}
-                      Location?
-                    </span>
+              <input
+                type="text"
+                id="treeAmount"
+                onChange={(e) =>
+                  setUserData({ ...userData, totalTrees: e.target.value })
+                }
+                placeholder="Enter tree planted amount?"
+                className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+              />
+            </label>
+            <label
+              htmlFor="treeAmount"
+              className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+            >
+              <span className="text-xs font-medium text-gray-700">
+                {" "}
+                Location?
+              </span>
 
-                    <input
-                      type="text"
-                      id="treeAmount"
-                      onChange={(e) =>
-                        setUserData({ ...userData, location: e.target.value })
-                      }
-                      placeholder="Where were the trees planted?"
-                      className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-                    />
-                  </label>
-                  <label
-                    for="plantingExperience"
-                    className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
-                  >
-                    <span className="text-xs font-medium text-gray-700">
-                      {" "}
-                      Planting Experience?
-                    </span>
+              <input
+                type="text"
+                id="treeAmount"
+                onChange={(e) =>
+                  setUserData({ ...userData, location: e.target.value })
+                }
+                placeholder="Where were the trees planted?"
+                className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+              />
+            </label>
+            <label
+              for="plantingExperience"
+              className="block overflow-hidden rounded-md border border-gray-200 px-3 py-2 shadow-sm focus-within:border-blue-600 focus-within:ring-1 focus-within:ring-blue-600"
+            >
+              <span className="text-xs font-medium text-gray-700">
+                {" "}
+                Planting Experience?
+              </span>
 
-                    <textarea
-                      type="text"
-                      id="treeExperience"
-                      rows={8}
-                      onChange={(e) =>
-                        setUserData({
-                          ...userData,
-                          description: e.target.value,
-                        })
-                      }
-                      placeholder="Talk about you planting experience?"
-                      className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
-                    ></textarea>
-                  </label>
-                </div>
+              <textarea
+                type="text"
+                id="treeExperience"
+                rows={8}
+                onChange={(e) =>
+                  setUserData({
+                    ...userData,
+                    description: e.target.value,
+                  })
+                }
+                placeholder="Talk about you planting experience?"
+                className="mt-1 w-full border-none p-0 focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+              ></textarea>
+            </label>
+          </div>
 
-                <div className="items-center gap-2 mt-3 sm:flex">
-                  <button
-                    className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
-                    onClick={() => setShowModal(false)}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    className="w-full mt-2 p-2.5 flex-1 text-white bg-indigo-600 rounded-md outline-none ring-offset-2 ring-indigio-600 focus:ring-2"
-                    onClick={() => handleSubmit()}
-                  >
-                    Post
-                  </button>
-                </div>
-              </div>
-            </div>
+          <div className="items-center gap-2 mt-3 sm:flex">
+            <button
+              className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
+              onClick={() => setShowModal(false)}
+            >
+              Cancel
+            </button>
+            <button
+              className="w-full mt-2 p-2.5 flex-1 text-white bg-indigo-600 rounded-md outline-none ring-offset-2 ring-indigio-600 focus:ring-2"
+              onClick={() => handleSubmit()}
+            >
+              Post
+            </button>
           </div>
         </div>
       </div>
-    </>
-  );
+    </div>
+  </div>;
 };
 
-export default AddSessionModal;
+export const SuccessForm = ({ session, totalTrees }) => {
+  <div className="flex items-center min-h-screen px-4 py-8">
+    <div className="relative max-w-lg mx-auto bg-white rounded-md shadow-lg">
+      <h4 className="text-lg p-4  bg-indigo-600 font-medium mb-4 text-center text-indigo-100">
+        Success
+      </h4>
+      <div className="mt-3 p-4 sm:flex">
+        <div className="mt-2 text-center sm:ml-4 sm:text-left">
+          <div className="flex justify-center flex-col space-y-3">
+            <span className="text-lg text-center mb-2 font-extrabold text-gray-700">
+              {" "}
+              Good job {session.user?.firstName}! You just planted {totalTrees}{" "}
+              trees! This prevents 30lb of CO2 entering the atmosphere.
+            </span>
+          </div>
+
+          <div className="items-center gap-2 mt-3 sm:flex">
+            <button
+              className="w-full mt-2 p-2.5 flex-1 text-gray-800 rounded-md outline-none border ring-offset-2 ring-indigo-600 focus:ring-2"
+              onClick={() => setShowModal(false)}
+            >
+              Close
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>;
+};
