287fab2a2ab257b76a8ed4b9bb4dcb7183d43b6e
jesse berkowitz <jberkow@brandeis.edu>
2022-10-29 23:06:53 -0700

    location functions added

--- pages/api/alternative_app.py
+++ pages/api/alternative_app.py
@@ -2,14 +2,17 @@ import json
 # d = {}
 # with open("DATA.json", "w") as outfile:
 #     json.dump(d, outfile)
-# school_data = {}
-# with open("School_DATA.json", "w") as outfile:
-#     json.dump(d, outfile)
+location_data = {}
+with open("Location_Data.json", "w") as outfile:
+    json.dump(location_data, outfile)
 
 class Student:
-    def __init__(self, teacher, trees):
+    # Student class adds to teacher, initiates teacher in json if teacher does not exist
+    def __init__(self, teacher, trees, text, location):
         self.teacher=teacher
         self.trees = trees
+        self.text = text
+        self.location = location  
     def add_tree(self):
         f = open('Data.json')
         Data = json.load(f)
@@ -19,9 +22,25 @@ class Student:
             Data[self.teacher]= self.trees
         with open("DATA.json", "w") as outfile:
             json.dump(Data, outfile)
+        f = open('Location_Data.json')
+
+        DATA = json.load(f)
+        if self.text not in DATA:
+            DATA[self.text]= []
+            DATA[self.text].append(self.location)          
+        else:
+            DATA[self.text].append(self.location)
+                    
+        
+        with open("Location_Data.json", "w") as outfile:
+            json.dump(DATA, outfile) 
+
+
         return
 
+
 class Teacher:
+    # Teacher class adds to school, pulls from student class, clears student values
     def __init__(self, name,school, trees):
         self.name = name
         self.school = school
@@ -48,9 +67,19 @@ class Teacher:
         with open("School_DATA.json", "w") as outfile:
             json.dump(Data, outfile)
         return
+    def clear_trees(self):
+        g = open('School_DATA.json')
+        Data = json.load(g)
+        Data[self.school]=0
+        with open("School_DATA.json", "w") as outfile:
+            json.dump(Data, outfile)
 
-s = Student('Terry', 10)
-s.add_tree()
+
+s1 = Student('Terry', 10, 'new tree', (324.2342,100))
+s2 = Student('Terry', 10, 'neww tree', (314.252,200))
+s1.add_tree()
+s2.add_tree()
+s2.add_tree()
 t = Teacher('Terry', 'James Woods High', 10)
 t.add_tree()
 
